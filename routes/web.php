3824688ca02c2b5ce3c6e8568683d447cc9a6c3c
congvt305 <vtcong305@gmail.com>
2020-01-23 05:50:34 +0700

    ..

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,33 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('customer')->group(function () {
+    Route::get('index', function () {
+        return view('modules.customers.index');
+    });
+
+    Route::get('create', function () {
+        //form create customer
+    });
+
+    Route::post('store', function () {
+        //xu ly luu du lieu tao khach hang
+    });
+
+    Route::get('{id}/show', function () {
+        //hien thi thong tin chi tiet khach hang cung co ma mac dinh id
+    });
+
+    Route::get('{id}/edit', function () {
+        //form chinh sua tt khach hang
+    });
+
+    Route::patch('{id}/update', function () {
+        //xu ly luu du lieu thông tin khách hàng được chỉnh sửa thông qua patch từ form
+    });
+
+    Route::delete('{id}', function () {
+        //xoa khach hang
+    });
+});
